a20dd25714afb10a5e7af666ca9c49cfc50abebf
Bui Tat Nhut Minh <minhbui88888888@gmail.com>
2025-03-07 00:28:20 +0700

    Add: Booking Repository

---
+++ BookingService/src/main/java/bookingservice/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package bookingservice.repository;
+
+import bookingservice.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByUserId(Long userId);
+}
