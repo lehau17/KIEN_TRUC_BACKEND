97555f8c4c17c072f48292d645ac282d98f7f063
team-meio <phamhuyentrang562003@gmail.com>
2025-04-04 17:42:03 +0700

    FEA : handle error handler

--- room-service/index.js
+++ room-service/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { ketNoiDatabase } = require("./src/config")
 const mainRouter = require("./src/router")
+const { ZodError } = require("zod")
 require('dotenv').config()
 
 
@@ -15,9 +16,38 @@ app.use("/api", mainRouter)
 //
 
 app.use((err, req, res, next) => {
-    console.log("=>>>>>>> Error Middleware: ", err);
-    res.status(err.status || 500).json({ isSuccess: false, message: err.message || "Internal Server Error" });
+    console.log("=>>>>>>> Error Middleware:", err);
+    // ✅ Xử lý lỗi Zod
+    if (err instanceof ZodError) {
+        // const validationErrors = err.errors.map(e => ({
+        //     field: e.path.join("."),
+        //     code: e.message,
+        //     message: ErrorCodeMap[e.message]?.message || e.message,
+        //   }));
+
+        const firstError = err.errors[0];
+        return res.status(400).json({
+            isSuccess: false,
+            message: firstError.message || "Validation error",
+            code: firstError.message,
+            field: firstError.path.join("."),
+        });
+    }
+
+    if (err.status && err.message) {
+        return res.status(err.status).json({
+            isSuccess: false,
+            message: err.message,
+            code: err.code || "ERROR",
+        });
+    }
+
+    return res.status(500).json({
+        isSuccess: false,
+        message: "Internal Server Error",
+    });
 });
+
 
 app.listen(3000, async () => {
     await ketNoiDatabase()
