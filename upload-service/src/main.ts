e91059316659be0279435f663ef1cf0fc41cfe36
lehau17 <haule@hitek.com.vn>
2025-05-17 21:48:45 +0700

    FEA : add upload service using s3 and minIO replace for S3

---
+++ upload-service/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors();
+
+    await app.listen(9999, () => {
+        console.log('🚀 Server is running on http://localhost:9999');
+    });
+}
+bootstrap();
---
+++ upload-service/src/upload/upload.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, Inject, HttpStatus } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import { UploadApiResponse, UploadApiErrorResponse } from 'cloudinary';
+import * as fs from 'fs';
+import { RpcException } from '@nestjs/microservices';
+@Injectable()
+export class UploadService {
+  constructor(@Inject('CLOUDINARY') private cloudService: typeof cloudinary) {}
+
+  async uploadFile(
+    file: any,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    try {
+      const folderPath = './uploads';
+      const filePath = `${folderPath}/${Date.now()}_${file.originalname}`;
+
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+      }
+      fs.writeFileSync(filePath, Buffer.from(file.buffer.data, 'base64'));
+      const isSuccess = await this.cloudService.uploader.upload(filePath, {
+        resource_type: 'video',
+      });
+      if (!isSuccess) {
+        throw new RpcException({
+          message: 'Upload file failed',
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
+      }
+      fs.rm(filePath, (err) => {
+        if (err) {
+          console.log('Xóa thất bại', err);
+        }
+      });
+
+      return isSuccess;
+    } catch (error) {
+      throw new RpcException({
+        message: 'Upload file failed',
+        statusCode: HttpStatus.BAD_REQUEST,
+      });
+    }
+  }
+}
